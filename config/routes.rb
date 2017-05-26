592a6af7d48d016bde1f38400e8274ec21644b34
gus-benitez <ing.benitez.gustavo@gmail.com>
2017-05-26 17:14:39 -0300

    Arreglo en la visualización de las validaciones. Y estetica de gender y mas...

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   resources :customers
 
   get 'cities/:state', to: 'application#cities'
-  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'customers#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
